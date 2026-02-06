a9eb1f91e1ab0255914e06d0a5d3425aabad10a8
Kausalyag <kausalya9g@gmail.com>
2026-02-06 20:29:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
-function saveComplaint() {
+document.getElementById("complaintForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
   let name = document.getElementById("name").value;
   let location = document.getElementById("location").value;
   let complaint = document.getElementById("complaint").value;
@@ -13,4 +15,6 @@ function saveComplaint() {
   localStorage.setItem("complaintData", JSON.stringify(data));
 
   alert("Complaint submitted successfully!");
-}
+
+  document.getElementById("complaintForm").reset();
+});
