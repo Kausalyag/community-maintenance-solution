1454172a2f7fe9dc3e5979a7e4d5e1d7b5276d47
Kausalyag <kausalya9g@gmail.com>
2026-02-06 20:19:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
-function submitted() {
+function saveComplaint() {
+  let name = document.getElementById("name").value;
+  let location = document.getElementById("location").value;
+  let complaint = document.getElementById("complaint").value;
+
+  let data = {
+    name: name,
+    location: location,
+    complaint: complaint,
+    status: "Pending"
+  };
+
+  localStorage.setItem("complaintData", JSON.stringify(data));
+
   alert("Complaint submitted successfully!");
 }
